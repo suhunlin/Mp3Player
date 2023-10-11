23b32edc124c5fc0071d0119c29207abac35e8e3
suhunlin <lin.suhun@msa.hinet.net>
2023-10-11 19:13:07 +0800

    Add logic about send READ_EXTERNAL_STORAGE permission request to user

--- app/src/main/java/com/suhun/mp3player/MainActivity.java
+++ app/src/main/java/com/suhun/mp3player/MainActivity.java
@@ -1,14 +1,51 @@
 package com.suhun.mp3player;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String tag = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(isSendUserPermissionAboutMp3Player()){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+        }else{
+            initMp3Player();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                initMp3Player();
+            }else{
+                finish();
+            }
+        }
+    }
+
+    private void initMp3Player(){
+
+    }
+
+    private boolean isSendUserPermissionAboutMp3Player(){
+        boolean result = false;
+
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+            return true;
+        }
+
+        return result;
     }
 }
